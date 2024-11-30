1092b1aa05193a1dfcd5ce7240ac716ffe717e9e
JohnGeoffreyA <23-75705@g.batstate-u.edu.ph>
2024-11-30 12:32:46 +0800

    inside main

--- rental.py
+++ rental.py
@@ -13,3 +13,38 @@ def main():
         print("\t\t\t|    [3] End Rent\t\t\t\t\t\t     |")
         print("\t\t\t|--------------------------------------------------------------------|")
         choice = int(input("\tEnter your choice: "))
+
+        if choice == 1:
+            apartment = Apartment()
+            apartment.rent_apartment(apartments) 
+            apartments[apartment.num] = apartment
+        elif choice == 2:
+            apartment_number = int(input("\n\tEnter the Apartment Number to Update: "))
+            if apartment_number in apartments:
+                apartments[apartment_number].update_rental()
+            else:
+                print("\tApartment not found!")
+        elif choice == 3:
+            apartment_number = int(input("\n\tEnter the Apartment Number to End Rent: "))
+            if apartment_number in apartments:
+                apartments[apartment_number].end_rent(apartments)  
+            else:
+                print("\tApartment not found!")
+        elif choice == 4:
+            if apartments:
+                print("\n\tRented Apartments:")
+                for apartment in apartments.values():
+                    apartment.view_apartments()
+
+                print("\n\tAvailable Apartments:")
+                for i in range(1, 31):
+                    if i not in apartments:
+                        print(f"\tApartment Number: {i} is available")
+            else:
+                print("\tAll apartments are available!")
+        elif choice == 5:
+            print("\n\tExiting the program. Goodbye!")
+            save_apartments(apartments)  
+            break
+        else:
+            print("\tInvalid choice. Please try again.")
