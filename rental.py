b5bfd0bb4bdbf77c401eae0bb002b624cf74e0af
JohnGeoffreyA <23-75705@g.batstate-u.edu.ph>
2024-11-30 12:04:53 +0800

    update rentals

--- rental.py
+++ rental.py
@@ -1,17 +1,4 @@
-        print("\tChoose Payment Option:")
-        print("\t[1] Full Payment")
-        print("\t[2] Down Payment")
-        payment_choice = int(input("\tEnter (1-2): "))
-
-        if payment_choice == 1:
-            print(f"\tTotal Payment Amount: ${self.payment}")
-            self.remaining_payment = 0
-            down_payment = float(input("\tEnter down payment amount: $"))
-            if down_payment > self.remaining_payment:
-                print(f"\tYour down payment exceeds the total rental price. Here's your change: ${down_payment - self.remaining_payment}")
-                down_payment = self.remaining_payment
-            self.remaining_payment -= down_payment
-            print(f"\tRemaining Payment: ${self.remaining_payment}")
-
-        print(f"\n\tYour rental has been successfully confirmed!")
-        print(f"\tYour rental end date is: {self.end_month}/{self.day}/{self.end_year}")
+    def update_rental(self):
+        self.name = input("\n\tEnter new Tenant Name: ")
+        self.month, self.day, self.year = map(int, input("\tEnter new Rent Date (Month Day Year): ").split())
+        print(f"\tUpdated rental information for apartment {self.num}.")
