ff5c98b1c7834586847b0009f72739a0aa4bdbe3
JohnGeoffreyA <23-75705@g.batstate-u.edu.ph>
2024-11-30 12:27:00 +0800

    main

--- rental.py
+++ rental.py
@@ -1,2 +1,16 @@
 import sys
 import json
+
+def main():
+    apartments = load_apartments()  # Load existing apartments data from the JSON file
+    while True:
+        # Menu options
+        print("\n\t\t\t|--------------------------------------------------------------------|")
+        print("\t\t\t|                            Apartment Rentals                       |")
+        print("\t\t\t|--------------------------------------------------------------------|")
+        print("\t\t\t|\t\t\t\t\t\t\t\t     |")
+        print("\t\t\t|    [1] Rent Apartment\t\t        [4] View All Apartments      |")
+        print("\t\t\t|    [2] Update Rental Information\t[5] Exit                     |")
+        print("\t\t\t|    [3] End Rent\t\t\t\t\t\t     |")
+        print("\t\t\t|--------------------------------------------------------------------|")
+        choice = int(input("\tEnter your choice: "))
